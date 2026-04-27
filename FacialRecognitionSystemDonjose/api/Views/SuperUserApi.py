931395517edd19f1cc21175547a0ec16eb7b8294
Dominic-Bravo <dominicianbravo017@gmail.com>
2026-04-27 20:09:59 +0800

    sync

--- FacialRecognitionSystemDonjose/api/Views/SuperUserApi.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import generics
-from api.serializer.SuperUser import SuperUserSerializer
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-class SuperUserCreateView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = SuperUserSerializer
